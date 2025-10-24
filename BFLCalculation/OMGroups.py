Fix some things about BFL calculation

BFLCalculation/OMGroups.py
@@ -182,6 +182,7 @@ class STWTakeoffAnalysisGroup(om.Group):
             allow_none=True,
             desc="List of variables to exclude from the indepvarcomp. Use this for variables that will instead come from the outputs of other components.",
         )
+        self.options.declare("transition_method", values=["ode", "simplified"], default="ode")
         self.callCounter = 0
 
     def setup(self):
@@ -211,7 +212,9 @@ class STWTakeoffAnalysisGroup(om.Group):
 
         self.add_subsystem(
             "analysis",
-            TakeoffAnalysis(num_nodes=nn, aircraft_model=STWAircraftModel, transition_method="ode"),
+            TakeoffAnalysis(
+                num_nodes=nn, aircraft_model=STWAircraftModel, transition_method=self.options["transition_method"]
+            ),
             promotes_inputs=["*"],
             promotes_outputs=["*"],
         )
@@ -220,7 +223,7 @@ class STWTakeoffAnalysisGroup(om.Group):
         self.nonlinear_solver = om.NewtonSolver(
             iprint=2,
             solve_subsystems=True,
-            maxiter=40,
+            maxiter=100,
             err_on_non_converge=True,
             reraise_child_analysiserror=True,
             restart_from_successful=True,
@@ -232,14 +235,17 @@ class STWTakeoffAnalysisGroup(om.Group):
         if self.callCounter == 0:
             nn = self.options["num_nodes"]
             knTomps = 0.514444  # Conversion factor from knots to m/s
+            v0Guess = 1.0 * knTomps
+            v1Guess = 120.0 * knTomps
+            vrGuess = 140.0 * knTomps
             # Initial guesses for takeoff speeds to help with convergence
-            outputs["v0v1.fltcond|Utrue"][:] = np.linspace(1.0, 140.0, nn) * knTomps
-            outputs["v1vr.fltcond|Utrue"][:] = np.linspace(140.0, 150.0, nn) * knTomps
-            outputs["v1v0.fltcond|Utrue"][:] = np.linspace(140.0, 1.0, nn) * knTomps
+            outputs["v0v1.fltcond|Utrue"][:] = np.linspace(v0Guess, v1Guess, nn)
+            outputs["v1vr.fltcond|Utrue"][:] = np.linspace(v1Guess, vrGuess, nn)
+            outputs["v1v0.fltcond|Utrue"][:] = np.linspace(vrGuess, v0Guess, nn)
 
             # Need these if using ODE transition method
             try:
-                outputs["rotate.fltcond|Utrue"][:] = np.linspace(100.0, 100.0, nn) * knTomps
+                outputs["rotate.fltcond|Utrue"][:] = np.linspace(v1Guess, v1Guess, nn)
                 outputs["rotate.accel_vert"][:] = np.linspace(0.05, 1.0, nn)
             except KeyError:
                 pass  # Not using the ODE transition method
@@ -270,7 +276,7 @@ if __name__ == "__main__":
     # - "ac|geom|wing|taper"
     # - "ac|geom|wing|toverc"
     # - "ac|weights|MTOW"
-    prob.model = STWTakeoffAnalysisGroup(num_nodes=numNodes, ivc_excludes=["ac|weights|MTOW"])
+    prob.model = STWTakeoffAnalysisGroup(num_nodes=numNodes, ivc_excludes=["ac|weights|MTOW"], transition_method="ode")
 
     prob.driver = om.ScipyOptimizeDriver()
     prob.driver.options["optimizer"] = "SLSQP"
@@ -279,7 +285,7 @@ if __name__ == "__main__":
     prob.model.add_design_var("ac|aero|takeoff_flap_deg", lower=0, upper=20, units="deg")
     prob.model.add_objective("bfl.distance_continue", scaler=1e-3, units="ft")  # Minimize balanced field length
 
-    prob.setup()
+    prob.setup(force_alloc_complex=True)
     prob.final_setup()
 
     # NOTE: It looks like there are some issues with the OpenConcept takeoff model not converting properly between true
@@ -303,7 +309,7 @@ if __name__ == "__main__":
     # Set an initial guess for the takeoff flap setting away from te upper bound
     prob.set_val("ac|aero|takeoff_flap_deg", 20.0, units="deg")
 
-    prob.set_val("ac|geom|wing|S_ref", 46.1274)
+    prob.set_val("ac|geom|wing|S_ref", 2 * 46.1274)
     prob.set_val("ac|geom|wing|AR", 8.48926)
     prob.set_val("ac|geom|wing|c4sweep", np.deg2rad(25.4682))
     prob.set_val("ac|geom|wing|taper", 0.309707)
@@ -311,11 +317,21 @@ if __name__ == "__main__":
     prob.set_val("ac|weights|MTOW", 57055.8)
 
     prob.run_model()
-    exit(0)
+    of = ["bfl.distance_continue", "bfl.distance_abort"]
+    wrt = [
+        "ac|geom|wing|S_ref",
+        "ac|geom|wing|AR",
+        "ac|geom|wing|c4sweep",
+        "ac|geom|wing|taper",
+        "ac|geom|wing|toverc",
+        "ac|weights|MTOW",
+    ]
+    prob.check_totals(of=of, wrt=wrt, step=1e-100, method="cs", compact_print=True, step_calc="abs")
 
-    prob.run_driver()
+    # prob.run_driver()
 
-    prob.run_model()
+    # prob.run_model()
+
     om.n2(prob, show_browser=False, outfile="takeoff_analysis_n2.html")
 
     # =============== Print some useful outputs ================
@@ -323,7 +339,12 @@ if __name__ == "__main__":
         {"var": "ac|weights|MTOW", "name": "MTOW", "units": "kg"},
         {
             "var": "bfl.distance_continue",
-            "name": "Balanced field length",
+            "name": "Balanced field takeoff length",
+            "units": "ft",
+        },
+        {
+            "var": "bfl.distance_abort",
+            "name": "Balanced field abort length",
             "units": "ft",
         },
         {"var": "takeoff|v1", "name": "V1 speed", "units": "kn"},
@@ -364,7 +385,7 @@ if __name__ == "__main__":
 
     # Label the start, decision and end points on the x axis
     xTicks = [0]
-    for phase in ["v0v1", "rotate"]:
+    for phase in ["v0v1", "rotate", "v1v0"]:
         xTicks.append(prob.get_val(f"{phase}.range", units="ft")[-1])
     for ax in takeoff_axs:
         ax.set_xticks(xTicks)

runscripts/aeroStructRun-MultipointParallel.py
@@ -981,9 +981,7 @@ if structOnlyOpt or args.task == "trim":
     if ptID == 0:
         # For the trim task we setup a dummy objective that doesn't depend on the trim variables so that the optimiser
         # just satisfies the trim constraints
-        flightPointProb.model.add_objective(
-            f"{localFlightPoint.name}.mass", scaler=1e-3, cache_linear_solution=True
-        )
+        flightPointProb.model.add_objective(f"{localFlightPoint.name}.mass", scaler=1e-3, cache_linear_solution=True)
 elif args.optType == "fuelburn":
     if isCruisePoint:
         flightPointProb.model.add_objective("totalFuelBurn", scaler=1e-4, cache_linear_solution=True)
@@ -1058,7 +1056,7 @@ if args.task != "trim":
     # This should only be applied if the flight point is a cruise point and the balanced field
     if isCruisePoint and args.includeBFL:
         flightPointProb.model.add_constraint(
-            "takeoff.rotate.range_final",
+            "takeoff.bfl.distance_continue",
             upper=args.maxBFL,
             scaler=1.0 / args.maxBFL,
             cache_linear_solution=True,
@@ -1265,6 +1263,7 @@ for output in flightPointProbOutputs:
         "avgToC",  # Average wing t/c
         "*CLmax_*",  # CLmax values (clean and flapped)
         "*takeoff|v*",  # Takeoff speeds
+        "*takeoff.bfl.*",  # Balanced field length speeds and distances
         "*ksFailure",  # TACS failure values
     ]
     for pattern in patterns:
@@ -1448,7 +1447,9 @@ MP.setObjCon(objCon)
 # Create wrapped functions to be used by multipoint sparse
 # ==============================================================================
 def procSetObj(x=None):
-    return runAeroStructAnalyses(x, evalFuncs=gradFuncs if args.task == "derivCheck" else dispFuncs, writeSolution=args.task != "opt")
+    return runAeroStructAnalyses(
+        x, evalFuncs=gradFuncs if args.task == "derivCheck" else dispFuncs, writeSolution=args.task != "opt"
+    )
 
 
 MP.addProcSetObjFunc("all", procSetObj)
@@ -1823,7 +1824,6 @@ if args.task in ["check", "opt", "trim", "derivCheck"]:
                     dill.dump(restartDict, f)
 
     elif args.task == "derivCheck":
-
         rng = np.random.default_rng(12345)
 
         # Check derivatives of all objectives and constraints with respect to all design variables
@@ -1835,9 +1835,9 @@ if args.task in ["check", "opt", "trim", "derivCheck"]:
 
         # FD settings
         stepSize = 1e-4
-        directionalThreshold = 3 # If a DV has a length greater than this we will test the directional derivative only
+        directionalThreshold = 3  # If a DV has a length greater than this we will test the directional derivative only
 
-        fdSens= {}
+        fdSens = {}
         for func in origFuncs:
             fdSens[func] = {}
 
@@ -1865,11 +1865,11 @@ if args.task in ["check", "opt", "trim", "derivCheck"]:
                 pertDVs[dvName] += dvPert
 
                 pertFuncs, _ = obj(pertDVs)
-                pertDVs[dvName] -= 2*dvPert
+                pertDVs[dvName] -= 2 * dvPert
                 pertFuncs2, _ = obj(pertDVs)
 
                 for func in origFuncs:
-                    fdSens[func][dvName][:, 0] = (pertFuncs[func] - pertFuncs2[func]) / (2*stepSize)
+                    fdSens[func][dvName][:, 0] = (pertFuncs[func] - pertFuncs2[func]) / (2 * stepSize)
             else:
                 for ii in range(numDV):
                     if ptRank == 0:
@@ -1879,11 +1879,11 @@ if args.task in ["check", "opt", "trim", "derivCheck"]:
                     pertDVs[dvName][ii] += dvPert
 
                     pertFuncs, _ = obj(pertDVs)
-                    pertDVs[dvName][ii] -= 2*dvPert
+                    pertDVs[dvName][ii] -= 2 * dvPert
                     pertFuncs2, _ = obj(pertDVs)
 
                     for func in origFuncs:
-                        fdSens[func][dvName][:, ii] = (pertFuncs[func] - pertFuncs2[func]) / (2*dvPert)
+                        fdSens[func][dvName][:, ii] = (pertFuncs[func] - pertFuncs2[func]) / (2 * dvPert)
 
         derivCheckData = {}
         for func in origFuncs:
@@ -1911,7 +1911,7 @@ if args.task in ["check", "opt", "trim", "derivCheck"]:
                     "fd": fd,
                 }
                 if analytic.shape != fd.shape:
-                    analyticSensProd = analytic @ (1/dvScales[dvName])
+                    analyticSensProd = analytic @ (1 / dvScales[dvName])
                     derivCheckData[func][dvName]["analyticProd"] = analyticSensProd
                     absError = analyticSensProd - fd.reshape(analyticSensProd.shape)
                     relError = absError / (np.abs(fd.reshape(analyticSensProd.shape)) + 1e-16)
@@ -1944,75 +1944,6 @@ if args.task in ["check", "opt", "trim", "derivCheck"]:
             rel_err_tol=1e-8 if isComplex else 1e-2,
             abs_err_tol=1e-8,
         )
-
-
-        # Define some groups of design variables
-        # structDesignVariables = ["dv_struct"] if args.addStructDVs else []
-        # aeroDesignVariables = [dvName for dvName in designVariables if "_AOA" in dvName]
-        # fuelDesignVariables = [dvName for dvName in designVariables if "-fuelMass" in dvName]
-        # geoDesignVariables = []
-        # geoInputs = ptComm.bcast(flightPointProb.model.geometry.list_inputs(out_stream=None), root=0)
-        # for dvName in designVariables:
-        #     for geoInput in geoInputs:
-        #         if geoInput[0] in dvName:
-        #             geoDesignVariables.append(dvName)
-        #             break
-        # wrt = geoDesignVariables + aeroDesignVariables + fuelDesignVariables  # + ["dv_struct"]
-        # fpName = localFlightPoint.name
-        # # of = [
-        #     # f"{fpName}.aero_post.cl",
-        #     # f"{fpName}.aero_post.cd",
-        #     # f"{fpName}.compliance",
-        #     # f"{fpName}.l_skin_ksFailure",
-        #     # "takeoff.rotate.range_final",
-        #     # "PlanformValues.QCSweep",
-        # # ]
-        # of = gradFuncs
-        # of = [f for f in of if f in flightPointProbOutputs]
-        # origDVs = {}
-        # for variable in wrt:
-        #     origDVs[variable] = flightPointProb.get_val(variable)
-
-        # with open(os.path.join(localOutputDir, f"{fpName}-derivCheck-{ptRank:03d}.pkl"), "wb") as pickleFile:
-        #     with open(
-        #         os.path.join(localOutputDir, f"{fpName}-derivCheck-{ptRank:03d}.txt"),
-        #         "w",
-        #     ) as textFile:
-        #         if ptRank == 0:
-        #             print(f"Testing derivatives of {of}, with respect to {wrt}", flush=True)
-        #         totalsCheckData = flightPointProb.check_totals(
-        #             of=of,
-        #             wrt=wrt,
-        #             method="cs" if isComplex else "fd",
-        #             form="central",
-        #             step=1e-200 if isComplex else 1e-3,
-        #             step_calc="abs",
-        #             out_stream=textFile,
-        #             compact_print=True,
-        #             rel_err_tol=1e-8 if isComplex else 1e-2,
-        #             abs_err_tol=1e-8,
-        #         )
-        #         for variable in wrt:
-        #             flightPointProb.set_val(variable, origDVs[variable])
-        #         flightPointProb.run_model()
-        #         if ptRank == 0:
-        #             print(f"Testing derivatives of {of}, with respect to dv_struct", flush=True)
-        #         totalsCheckData.update(
-        #             flightPointProb.check_totals(
-        #                 of=of,
-        #                 wrt=["dv_struct"],
-        #                 method="cs" if isComplex else "fd",
-        #                 form="central",
-        #                 step=1e-200 if isComplex else 1e-5,
-        #                 step_calc="rel",
-        #                 out_stream=textFile,
-        #                 compact_print=True,
-        #                 rel_err_tol=1e-8 if isComplex else 1e-2,
-        #                 abs_err_tol=1e-8,
-        #                 directional=True,
-        #             )
-        #         )
-        #     dill.dump(totalsCheckData, pickleFile, protocol=-1)
 
 # --- Write out the DVs and outputs that aren't too long (e.g not the ADflow state vector) in unscaled form to a pickle file ---
 outputs = flightPointProb.model.list_outputs(